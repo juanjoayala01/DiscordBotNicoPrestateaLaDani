de306f9eeee56bc698dc3c207d63abbc88c2e8ad
Juanjo <42329001+juanjoayala01@users.noreply.github.com>
2021-11-18 14:57:48 -0400

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -33,7 +33,7 @@ module.exports = {
     id: "Main",
     host: "discordbotprovisorio.herokuapp.com/",
     port: 443, // The port that lavalink is listening to. This must be a number!
-    pass: "youshallnotpass",
+    pass: "456852jj",
     secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
